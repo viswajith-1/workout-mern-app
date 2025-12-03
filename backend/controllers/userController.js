df9e924ffee675cc51dfb6a8f4642ab329818167
Viswajith S B <121398392+viswajith-1@users.noreply.github.com>
2025-12-03 21:25:31 +0530

    jwt fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,19 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET , {
+    expiresIn: "3d",
+  });
+};
+
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.login(email, password);
-    res.status(200).json({ user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -15,12 +22,9 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const createToken = (_id) => {
-      return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: "3d" });
-    };
+    const user = await User.signup(email, password);
     const token = createToken(user._id);
-    const user = await User.signup(email, token);
-    res.status(201).json({ user });
+    res.status(201).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
